fca74a9f6ca9178d7fe7a0c6728b6d1150d6823f
Kolton Gagnon <Kolton.Gagnon@Gmail.com>
2018-07-31 18:30:38 -0300

    Create and include the main component into the main runner file

--- client/reduxstagram.js
+++ client/reduxstagram.js
@@ -1,1 +1,11 @@
-// Let's go
+// Library imports
+import React from 'react';
+import { render } from 'react-dom';
+
+// Components imports
+import Main from './components/Main';
+
+// Importing styles for webpack. Webpack will insert the style tag in the index.html file
+import CSS from './styles/style.styl'; 
+
+render(<Main/>, document.getElementById('root'));
