a953b1eabf5b571e4922e20261598e1d65131865
Kolton Gagnon <Kolton.Gagnon@Gmail.com>
2018-08-01 17:56:23 -0300

    Create the root reducer

--- client/reducers/index.js
+++ client/reducers/index.js
@@ -1,8 +1,11 @@
 /**
  * Root Reducer
  * Only once reducer can be included in Redux so creating a root reducer and including all the reducers is the only way to do so. This is what we are doing in this file, we are including the posts and comments reducers and merging them with the state.
+ * Note that reducers are the only one that does "editing of state"
+ * 
+ * Note that all reducers will run after each event is fired. It is up to the reducer to chose to act on that action and perform a tasks.
  */
-import { combinedReducers } from 'redux';
+import { combineReducers } from 'redux';
 import { routerReducer } from 'react-router-redux';
 
 import posts from './posts';
@@ -11,6 +14,10 @@ import comments from './comments';
 /**
  * There are three things that will live in our state, the post, the comment and the route changes.
  */
-const rootReducer = combinedReducers({posts, comments, router: routerReducer})
+const rootReducer = combineReducers({
+  posts, 
+  comments, 
+  routing: routerReducer
+});
 
 export default rootReducer;
