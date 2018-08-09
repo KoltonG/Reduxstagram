3ffc21a06c6a4c61d0cf89b917c4d9e1f72682d2
Kolton Gagnon <Kolton.Gagnon@Gmail.com>
2018-08-09 15:20:14 -0300

    Update the Single component by including Photo and Comments component

--- client/components/Single.js
+++ client/components/Single.js
@@ -1,10 +1,22 @@
 import React from 'react';
+import Photo from './Photo';
+import Comments from './Comments';
 
 class Single extends React.Component {
   render() {
+    // Get the post index by looking through all posts and finding the post with the matching params code.
+    const postIndex = this.props.posts.findIndex((post) => post.code === this.props.params.postId);
+    
+    // Get the post itself
+    const post = this.props.posts[postIndex];
+
+    // Get the comments for the post
+    const postComments = this.props.comments[post.code] || [];
+    
     return (
       <div className='single-photo'>
-        This is the single component
+        <Photo index={postIndex} post={post} {...this.props} />
+        <Comments postComments={postComments} />
       </div>
     )
   }
